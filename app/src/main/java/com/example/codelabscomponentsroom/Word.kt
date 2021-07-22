2495cf13fe3e0bbd8d0b43778e1f66080538057d
Delong90 <matushkin030790@gmail.com>
2021-07-22 21:15:56 +0300

    init

---
+++ app/src/main/java/com/example/codelabscomponentsroom/Word.kt
@@ -0,0 +1,18 @@
+package com.example.codelabscomponentsroom
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+//@Entity(tableName = "word_table")
+//class Word(@PrimaryKey @ColumnInfo(name = "word") val word: String)
+
+
+@Entity(tableName = "word_table")
+
+class Word(
+
+    @PrimaryKey(autoGenerate = true) val id: Int,
+
+    @ColumnInfo(name = "word") val word: String
+)
